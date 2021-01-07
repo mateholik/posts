b11969d10d54a560cd9c7df76f2e69ad4d1dc225
mateholik <vaisov.vladimir@gmail.com>
2021-01-07 16:07:48 +0200

    middleware

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import { createStore, compose, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import { rootReducer } from "./redux/rootReducer";
 import thunk from "redux-thunk";
+import { spamMiddleware } from "./redux/middleware";
 
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunk),
+    applyMiddleware(thunk, spamMiddleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
